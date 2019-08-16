8dc051f8d5f5009a624e1c2686e037f4f88fd9f9
BuildTools <arthurpisakhov1@gmail.com>
2019-08-15 20:54:43 -0400

    this questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. pronoun for js object
+* 2. objects context
+* 3. where/when function is called
+* 4. 4 bindings
 *
 * write out a code example of each explanation above
 */
@@ -13,9 +13,13 @@
 
 // code example for Window Binding
 
+// console.log(this)
+
 // Principle 2
 
 // code example for Implicit Binding
+
+
 
 // Principle 3
 
